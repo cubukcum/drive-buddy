4021919b0f9750efc822d407390fd10980fbf786
yusuf kemal pınarcı <yusufkmlpinarci@gmail.com>
2024-04-14 06:06:19 +0300

    kotlin dosyaları

---
+++ app/src/main/java/com/example/drive_buddy/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.drive_buddy
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onButtonClickedd(view: View) {
+        // Yol kayıt tuşuna basılınca CameraActivity'e yönlendirme
+        val intent = Intent(this, CameraActivity::class.java)
+        startActivity(intent)
+    }
+}
