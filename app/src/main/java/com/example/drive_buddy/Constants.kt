e4a1056c583ed7ac243fa073cce791cb81265de3
Mehmet Sait Cubukcu <109916532+cubukcum@users.noreply.github.com>
2024-04-15 12:31:45 +0300

    updated app name new model for better response time ~250ms

--- app/src/main/java/com/example/drive_buddy/Constants.kt
+++ app/src/main/java/com/example/drive_buddy/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.drive_buddy
 
 object Constants {
-    const val MODEL_PATH = "CarDetectionModel.tflite"
-    const val LABELS_PATH = "labels.txt"
+    const val MODEL_PATH = "yolov8n_int8.tflite"
+    const val LABELS_PATH = "labelsCopy.txt"
 }
