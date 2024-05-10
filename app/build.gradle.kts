8568f817aadc5b1e2b65459dbade522bc021890f
Mehmet Sait Cubukcu <109916532+cubukcum@users.noreply.github.com>
2024-05-10 16:19:05 +0300

    added opencv and detection model files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(project(":opencvsdk490"))
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
     val camerax_version = "1.3.0"
     implementation("androidx.camera:camera-core:${camerax_version}")
     implementation("androidx.camera:camera-camera2:${camerax_version}")
@@ -56,8 +59,6 @@ dependencies {
     implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
     implementation("org.tensorflow:tensorflow-lite-task-vision:+")
 
-//    implementation("org.openpnp:opencv:4.5.1-2")
-    implementation("com.quickbirdstudios:opencv:3.4.15")
     implementation("org.apache.commons:commons-math3:3.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.2")
 
